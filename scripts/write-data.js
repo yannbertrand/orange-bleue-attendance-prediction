53fb0c1288ea57da9123959815606b50ae600f7a
Yann Bertrand <5855339+yannbertrand@users.noreply.github.com>
2025-11-10 19:03:38 +0100

    🔥 Remove unused import

--- scripts/write-data.js
+++ scripts/write-data.js
@@ -1,6 +1,5 @@
 import { writeFile } from 'node:fs';
 import attendance from '../data/attendance.json' with { type: 'json' };
-import { getAttendanceLiveNumber } from './get-attendance-live-number.js';
 
 export const updateAttendanceFile = async (newAttendanceLiveData) => {
 	const allTrips = [...attendance, newAttendanceLiveData];
