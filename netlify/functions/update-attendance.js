4b98d0393b793344006e2ccdd3fa1e46041fa445
Yann Bertrand <5855339+yannbertrand@users.noreply.github.com>
2025-11-24 12:24:51 +0100

    ⚡️ Run every 5 minutes

--- netlify/functions/update-attendance.js
+++ netlify/functions/update-attendance.js
@@ -4,7 +4,7 @@ import { getAttendanceLiveNumber } from '../../scripts/get-attendance-live-numbe
 import { getTodayCourses } from '../../scripts/get-today-courses.js';
 
 export const config = {
-  schedule: '* * * * *',
+  schedule: '*/5 * * * *',
 };
 
 export default async () => {
