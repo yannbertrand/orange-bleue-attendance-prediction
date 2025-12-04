99c659d148a46c7e283f1fcc402255543b84ea0d
Yann Bertrand <5855339+yannbertrand@users.noreply.github.com>
2025-12-04 21:23:56 +0100

    🔥 Stop storing second blobs

--- src/io/write-netlify-data.js
+++ src/io/write-netlify-data.js
@@ -1,15 +1,13 @@
 import { store } from './read-netlify-data.js';
 
 export async function setNetlifyEvent(event) {
-  const dateString = event.date.toString();
-
   const eventDay =
     event.date.hour < 5 ? event.date.subtract({ day: 1 }) : event.date;
   const dayString = eventDay.toPlainDate().toString();
   const netlifyContent = await store.get(dayString, { type: 'json' });
 
   if (event.courseParticipants !== undefined) {
-    await store.setJSON(dayString, [
+    return await store.setJSON(dayString, [
       ...(netlifyContent ?? []),
       {
         ...event,
@@ -17,15 +15,7 @@ export async function setNetlifyEvent(event) {
         endDateTime: event.endDateTime.toString(),
       },
     ]);
-
-    return await store.setJSON(dateString, {
-      ...event,
-      startDateTime: event.startDateTime.toString(),
-      endDateTime: event.endDateTime.toString(),
-    });
   } else {
-    await store.setJSON(dayString, [...(netlifyContent ?? []), event]);
-
-    return await store.setJSON(dateString, event);
+    return await store.setJSON(dayString, [...(netlifyContent ?? []), event]);
   }
 }
