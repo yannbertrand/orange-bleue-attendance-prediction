88136ca0d15761f9de90ad7b795fbcb34501c222
Yann Bertrand <5855339+yannbertrand@users.noreply.github.com>
2025-12-04 20:58:10 +0100

    ✨ Store netlify blobs in day array

--- src/io/write-netlify-data.js
+++ src/io/write-netlify-data.js
@@ -2,18 +2,30 @@ import { store } from './read-netlify-data.js';
 
 export async function setNetlifyEvent(event) {
   const dateString = event.date.toString();
-  const baseEvent = {
-    ...event,
-    date: dateString,
-  };
+
+  const eventDay =
+    event.date.hour < 5 ? event.date.subtract({ day: 1 }) : event.date;
+  const dayString = eventDay.toPlainDate().toString();
+  const netlifyContent = await store.get(dayString, { type: 'json' });
 
   if (event.courseParticipants !== undefined) {
+    await store.setJSON(dayString, [
+      ...(netlifyContent ?? []),
+      {
+        ...event,
+        startDateTime: event.startDateTime.toString(),
+        endDateTime: event.endDateTime.toString(),
+      },
+    ]);
+
     return await store.setJSON(dateString, {
-      ...baseEvent,
+      ...event,
       startDateTime: event.startDateTime.toString(),
       endDateTime: event.endDateTime.toString(),
     });
   } else {
-    return await store.setJSON(dateString, baseEvent);
+    await store.setJSON(dayString, [...(netlifyContent ?? []), event]);
+
+    return await store.setJSON(dateString, event);
   }
 }
