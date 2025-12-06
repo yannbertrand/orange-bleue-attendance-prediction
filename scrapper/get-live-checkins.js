9d3a456c3be092952a639e9786595324d7f60dee
Yann Bertrand <5855339+yannbertrand@users.noreply.github.com>
2025-12-06 01:15:59 +0100

    ✨ Get more checkouts

--- scrapper/get-live-checkins.js
+++ scrapper/get-live-checkins.js
@@ -41,7 +41,7 @@ export async function getLiveCheckins() {
 export async function getLiveCheckouts() {
   const { studioId, cookie } = getMagicLineInfo();
 
-  const response = await fetch(
+  const response1 = await fetch(
     `https://lob.web.magicline.com/rest-api/checkout?organizationUnitId=${studioId}&offset=0&maxResults=100&search=&filter=&sortedby=checkinTime&checkouts=false&direction=DESCENDING&showBadges=false`,
     {
       credentials: 'include',
@@ -66,11 +66,40 @@ export async function getLiveCheckouts() {
     }
   );
 
-  if (!response.ok) {
-    throw new Error(`${response.status} ${response.statusText}`);
+  const response2 = await fetch(
+    `https://lob.web.magicline.com/rest-api/checkout?organizationUnitId=${studioId}&offset=100&maxResults=100&search=&filter=&sortedby=checkinTime&checkouts=false&direction=DESCENDING&showBadges=false`,
+    {
+      credentials: 'include',
+      headers: {
+        'User-Agent':
+          'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:146.0) Gecko/20100101 Firefox/146.0',
+        Accept: 'application/json, text/javascript, */*; q=0.01',
+        'Accept-Language': 'fr,en;q=0.5',
+        'X-ML-WC-VERSION': '3.458.5',
+        'X-Requested-With': 'XMLHttpRequest',
+        'Sec-GPC': '1',
+        'Sec-Fetch-Dest': 'empty',
+        'Sec-Fetch-Mode': 'cors',
+        'Sec-Fetch-Site': 'same-origin',
+        Pragma: 'no-cache',
+        'Cache-Control': 'no-cache',
+        Cookie: cookie,
+      },
+      referrer: 'https://lob.web.magicline.com/',
+      method: 'GET',
+      mode: 'cors',
+    }
+  );
+
+  if (!response1.ok) {
+    throw new Error(`${response1.status} ${response1.statusText}`);
+  }
+  if (!response2.ok) {
+    throw new Error(`${response2.status} ${response2.statusText}`);
   }
 
-  const result = await response.json();
+  const result1 = await response1.json();
+  const result2 = await response2.json();
 
-  return result.map((c) => getCustomer(c));
+  return [...result1, ...result2].map((c) => getCustomer(c));
 }
